0a800a95df8bc3a132ed506139fd9146d1b489de
Mark Antabi <121069920+hexmarsh@users.noreply.github.com>
2026-09-18 21:56:27 -0400

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        std::unordered_map<int, int> indices;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            int diff = target - nums[i];
+
+            if (indices.find(diff) != indices.end()) {
+                return {indices[diff], i};
+            }
+
+            indices[nums[i]] = i;
+        }
+
+        return {};
+    }
+};
